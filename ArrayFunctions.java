6d617810e416680468ea64fac5304390ee0ab875
Suganya M <suganya.m13102000@gmail.com>
2026-04-08 17:57:10 +0530

    Add files via upload

---
+++ ArrayFunctions.java
@@ -0,0 +1,111 @@
+package ARRAY_Example;
+import java.util.Scanner;
+
+public class ArrayFunctions {
+	 static Scanner sc = new Scanner(System.in);
+	 static int n;
+	 static int arr[];
+
+public static void ArraySize() {
+	System.out.println("Enter the size of array : ");
+	n = sc.nextInt();
+	System.out.println("Array size : " + n );																		
+    arr = new int[n];
+	
+	
+}
+public  static void getInput() {
+	for(int i=0;i<n;i++) {
+		System.out.print("arr[" + i + "] : ");
+		arr[i] = sc.nextInt();
+		}
+}
+public static void display() {
+	for(int i =0;i<n;i++) {
+	 System.out.print(arr[i] + " ");
+	}
+	System.out.println();
+}
+public static void processArray() {
+
+    String even = "", odd = "", prime = "", palindrome = "", armstrong = "", evenPrime = "";
+
+    for(int num : arr) {
+
+        // Even / Odd
+        if(num % 2 == 0)
+            even = even + num + " ";
+        else
+            odd = odd + num + " ";
+
+        // Palindrome
+        int temp = num, rev = 0;
+        while(temp > 0) {
+            int rem = temp % 10;
+            rev = rev * 10 + rem;
+            temp = temp / 10;
+        }
+        if(rev == num && num > 9)
+            palindrome =  palindrome + num + " ";
+
+        // Armstrong
+        temp = num;
+
+     // Step 1: find length
+     int len = String.valueOf(num).length();
+
+     // Step 2: calculate power sum
+     int sum = 0;
+     while(temp > 0) {
+         int d = temp % 10;
+         sum = sum + (int)Math.pow(d, len);   // use power
+         temp = temp / 10;
+     }
+
+     // Step 3: check
+     if(sum == num && num >= 100)
+         armstrong = armstrong + num + " ";
+     
+        // Prime
+        if(num > 1) {
+            int f = 0;
+            for(int j = 1; j <= num; j++) {
+                if(num % j == 0)
+                    f++;
+            }
+            if(f == 2) {
+                prime = prime + num + " ";
+
+                // Even Prime
+                if(num % 2 == 0)
+                    evenPrime = evenPrime + num + " ";
+            }
+        }
+    }
+        
+    
+
+    // Final Output
+    System.out.println("Odd: " + odd);
+    System.out.println("Even: " + even);
+    System.out.println("Prime: " + prime);
+    System.out.println("Even Prime: " + evenPrime);
+    System.out.println("Palindrome: " + palindrome);
+    System.out.println("Armstrong: " + armstrong);
+    
+}
+    
+
+
+	public static void main(String args[]) {
+		
+		ArraySize();
+		getInput();
+		display();
+		processArray();
+		
+		
+		
+	}
+
+}
